de581302300ca5ef5e5d4404b49c0e21627ecdff
SAKSHI1243 <sakshiisah123@gmail.com>
2025-07-17 11:10:47 +0530

    Refactor Node constructor for clarity

--- src/Recursion/NQueen.java
+++ src/Recursion/NQueen.java
@@ -1,19 +1,19 @@
 package Recursion;
 import java.util.*;
 public class NQueen {
-    public static boolean isSafe(char [][]board, int r, int c){
-        for(int j=0;j<board.length;j++){//rows
-            if(board[r][j]=='Q') return false;
+    public static boolean isSafe(char[][] board,int r, int c){
+        for (int j = 0; j < board[0].length; j++) {//rows
+            if(board[r][j]=='Q')return false;
         }
-        for(int i=0;i<board.length;i++){//cols
-            if(board[i][c]=='Q') return false;
+        for (int i = 0; i < board.length; i++) {//cols
+            if(board[i][c]=='Q')return false;
         }
         int i=r,j=c;
         while(i>=0 && j< board.length){//NE
             if (board[i][j]=='Q') return false;
             i--;j++;
         }
-         i=r;j=c;
+        i=r;j=c;
         while(i< board.length && j< board.length){//SE
             if (board[i][j]=='Q') return false;
             i++;j++;
@@ -24,35 +24,37 @@ public class NQueen {
             i++;j--;
         }
         i=r;j=c;
-        while(i>=0 && j>=0){//SW
+        while(i>=0 && j>=0){//NW
             if (board[i][j]=='Q') return false;
             i--;j--;
         }
         return true;
     }
-    public static void nQueen(char [][]board, int row){
-        if(row==board.length){
-            for (int i = 0; i <board.length ; i++) {
-                for (int j = 0; j < board[i].length; j++) System.out.print(board[i][j]+".");
-                System.out.println();
-            }
-            System.out.println();
+    public static void solve(char [][]board, int row,List<List<String>> res){
+        if(row== board.length){
+            List<String> curr= new ArrayList<>();
+            for (int i = 0; i < board.length ; i++) curr.add(new String(board[i]));
+            res.add(curr);
             return;
         }
         for (int i = 0; i < board.length; i++) {
-            if (isSafe(board,row,i)){
+            if(isSafe(board,row,i)){
                 board[row][i]='Q';
-                nQueen(board,row+1);
+                solve(board,row+1,res);
                 board[row][i]='.';
             }
         }
     }
-
-    public static void main(String[] args) {
-        char [][]board= new char[4][4];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) board[i][j]='.';
+    public static List<List<String>> nQueens(int n){
+        List<List<String>> res= new ArrayList<>();
+        char [][]board=new char[n][n];
+        for (int i=0;i<n;i++){
+            for (int j = 0; j < n; j++) board[i][j]='.';
         }
-        nQueen(board,0);
+        solve(board,0,res);
+        return res;
+    }
+    public static void main(String[] args) {
+        System.out.println(nQueens(4));
     }
 }
