acd03ceffc81c307967fa20fce4120fc762ed7dd
SAKSHI1243 <sakshiisah123@gmail.com>
2025-07-17 13:04:31 +0530

    Refactor Node constructor for clarity

--- src/Recursion/NQueenOptimized.java
+++ src/Recursion/NQueenOptimized.java
@@ -33,5 +33,36 @@ public class NQueenOptimized {
     }
     public static void main(String[] args) {
         System.out.println(nQueen(4));
+        System.out.println(nQueen2(4));//mtd2
+    }
+   // mtd2
+    public static List<List<Integer>> nQueen2(int n){
+        List<List<Integer>> res= new ArrayList<>();
+        int [][]board= new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)  board[i][j]=0;
+        }
+        boolean[] cols= new boolean[n];
+        boolean[] diag1= new boolean[2*n-1];
+        boolean[] diag2= new boolean[2*n-1];
+        List<Integer> curr= new ArrayList<>();
+        solve2(board,0,cols,diag1,diag2,res,curr);
+        return res;
+    }
+
+    public static void solve2(int [][]board,int row,boolean[]cols, boolean[]diag1, boolean[] diag2,List<List<Integer>> res,List<Integer> curr){
+        if(row==board.length){
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int col=0; col< board[0].length;col++){
+            int d1= row-col+ board.length-1, d2= row+col;
+            if(cols[col]||diag1[d1]||diag2[d2]) continue;
+            curr.add(col+1);
+            cols[col]=diag1[d1]=diag2[d2]=true;
+            solve2(board,row+1,cols,diag1,diag2,res,curr);
+            curr.remove(curr.size()-1);
+            cols[col]=diag1[d1]=diag2[d2]=false;
+        }
     }
 }
