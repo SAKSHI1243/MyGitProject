6fe220d430fd9c87a67374a0a5796234e3b326a9
sakshi sah <sakshiisah123@gmail.com>
2025-09-23 23:48:33 +0530

    Refactor Node constructor for clarity

--- src/ARRAYS/KadnesAlgo.java
+++ src/ARRAYS/KadnesAlgo.java
@@ -25,6 +25,16 @@ public class KadnesAlgo {
         System.out.println();
         return max;
     }
+    public static int findSum(int []ar){
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+        for (int i = 0; i < ar.length; i++) {
+            sum+=ar[i];
+            if (sum>max) max=sum;
+            if (sum<0) sum=0;
+        }
+        return max;
+    }
     public static void main(String[] args) {
     int []nums={-2,1,-3,4,-1,2,1,-5,4};  //{2,3,5,-2,7,-4};
     int x= maxSubarray(nums);
