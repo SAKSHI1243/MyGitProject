f7e6a5b04b3e5897052e5c2254af7898fccf85a6
sakshi sah <sakshiisah123@gmail.com>
2025-11-28 22:16:36 +0530

    Refactor Node constructor for clarity

--- src/GRAPHS/PrimsAlgorithm.java
+++ src/GRAPHS/PrimsAlgorithm.java
@@ -1,28 +1,29 @@
 package GRAPHS;
 import java.util.*;
 public class PrimsAlgorithm {
-    static class Pair{
-        int distance,node;
-        Pair(int distance, int node){
-            this.distance=distance;
+    public static class Pair{
+        int node;
+        int distance;
+        Pair(int node, int distance){
             this.node=node;
+            this.distance=distance;
         }
     }
-    public static int minimumWtSum(int v, ArrayList<ArrayList<ArrayList<Integer>>>adj){
+    public static int primsAlgo(int v, ArrayList<ArrayList<ArrayList<Integer>>>adj){
+        PriorityQueue<Pair>pq=new PriorityQueue<>((x,y)->x.distance-y.distance);
+        int vis[]= new int[v];
         int sum=0;
-        PriorityQueue<Pair>pq=new PriorityQueue<>((a,b)->a.distance- b.distance);
         pq.add(new Pair(0,0));
-        int []vis=new int[v];
         while (!pq.isEmpty()){
-            Pair x=pq.poll();
-            int wt=x.distance, node=x.node;
+            int wt=pq.peek().distance;
+            int node=pq.peek().node;
+            pq.poll();
             if(vis[node]==1) continue;
             vis[node]=1;
             sum+=wt;
-            for (int i=0;i<adj.get(node).size();i++){
-                int w=adj.get(node).get(i).get(1);
-                int n=adj.get(node).get(i).get(0);
-                if(vis[n]==0) pq.add(new Pair(w,n));
+            for (int i = 0; i < adj.get(node).size(); i++) {
+                int edW=adj.get(node).get(i).get(1), adjNode=adj.get(node).get(i).get(0);
+                if(vis[adjNode]==0) pq.add(new Pair(edW,adjNode));
             }
         }
         return sum;
@@ -44,6 +45,6 @@ public class PrimsAlgorithm {
             adj.get(u).add(tmp1);
             adj.get(v).add(tmp2);
         }
-        System.out.println(minimumWtSum(5,adj));
+        System.out.println(primsAlgo(5,adj));
     }
 }
